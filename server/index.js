6dc313b2373e393e59f2bafad6015448f6bbc089
unknown <hartman.william@gmail.com>
2018-05-11 10:12:59 -0700

    working on auth

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-const http = require('http');
-const path = require('path');
+// const http = require('http');
+// const path = require('path');
 const express = require('express');
 const axios = require('axios');
 const massive = require('massive');
@@ -8,6 +8,8 @@ const process = require("process");
 const bodyParser = require('body-parser');
 const cors = require('cors')
 const moment = require('moment');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
 require('dotenv').config();
 
 const app = express();
@@ -15,7 +17,6 @@ app.use(bodyParser.json());
 app.use(cors());
 //app.use(express.static(__dirname + './../build'));
 
-const SESSION_SECRET = process.env.SESSION_SECRET;
 
 // Use the session middleware
 massive(process.env.CONNECTION_STRING)
@@ -25,10 +26,63 @@ massive(process.env.CONNECTION_STRING)
 }).catch(err=>console.log(err))
  
 app.use(session({
-    secret: SESSION_SECRET, 
+    secret: process.env.SECRET, 
     cookie: { maxAge: 60000 },
     resave: false,
     saveUninitialized: true
    }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new Auth0Strategy({
+    domain: process.env.AUTH_DOMAIN,
+    clientID: process.env.AUTH_CLIENT_ID,
+    clientSecret: process.env.AUTH_CLIENT_SECRET,
+    callbackURL: process.env.AUTH_CALLBACK
+}, (accessToken, refreshToken, extraParams, profile, done) => {
+    const db = app.get("db");
+    const userData = profile._json;
+    db.find_user([userData.identities[0].user_id]).then(user => {
+    if (user[0]) {
+        return done(null, user[0].user_id);
+    } else {
+        db.create_user([
+            userData.name,
+            userData.email,
+            userData.identities[0].user_id
+        ])
+        .then(user => {
+            return done(null, user[0].user_id);
+        });
+    }
+    });
+}))
+
+passport.serializeUser( (profile, done) => {
+    done(null, profile);
+}) 
+
+passport.deserializeUser( (id, done) => {
+    app.get("db").find_session_user([id])
+        .then(user => {
+        console.log(user);
+        done(null, user[0]);
+        });
+})
+
+app.get('/auth', passport.authenticate('auth0'));
+app.get('/auth/callback', passport.authenticate('auth0', {
+    successRedirect: 'http://localhost:3000/',
+    failureRedirect: '/auth'
+}))
+
+app.get('/auth/me', (req, res) => {
+    console.log('auth/me endpoint hit')
+    if(!req.user){
+        return res.status(401).send('No user logged in.');
+    }
+    return res.status(200).send(req.user);
+})
 
 app.listen(process.env.PORT, () => console.log(`Listening on port: ${process.env.PORT}`));
--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 ReactDOM.render(
   <Provider store={store}>
-  <MuiThemeProvider>
-    <Router>
-      <App/>
-    </Router>
+    <MuiThemeProvider>
+        <Router>
+            <App/>
+        </Router>
     </MuiThemeProvider>
   </Provider>,
   document.getElementById('root'),
